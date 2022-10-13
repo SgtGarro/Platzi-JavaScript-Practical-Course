956da1bd4bc6e41eafea6b9383175ad18af61e86
SgtGarro <marcelogarro137@gmail.com>
2022-10-13 09:48:57 -0500

    addEventListener

--- main.js
+++ main.js
@@ -2,7 +2,14 @@ const h1 = document.querySelector("h1");
 const input1 = document.querySelector("#calc1");
 const input2 = document.querySelector("#calc2");
 const btn = document.querySelector("#btnCalc");
+const form = document.querySelector("#form");
 
-function onClick() {
-  console.log(Number(input1.value) + Number(input2.value));
+btn.addEventListener("click", addValues);
+
+// form.addEventListener("submit", addValues);
+
+function addValues(event) {
+  console.log({ event });
+  event.preventDefault();
+  const sumInpts = Number(input1.value) + Number(input2.value);
 }
